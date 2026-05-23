2e22ef20ea29594baacabbb93aee163db7d43eb3
SheshuNalla <sheshunalla002@gmail.com>
2026-05-23 13:00:29 +0530

    feat(contact): remove personal contact number

--- script.js
+++ script.js
@@ -43,13 +43,4 @@ function copyEmail(){
         alert.classList.add("opacity-0", "transition", "duration-300")
     },2500)
 }
-function copyNumber(){
-    const number = document.querySelector("#number").innerText;
-    const alert = document.querySelector(".copy-number")
-    navigator.clipboard.writeText(number);
-    alert.classList.remove("opacity-0");
-    setTimeout(()=>{
-        alert.classList.add("opacity-0", "transition", "duration-300")
-    },2500)
-}
 
